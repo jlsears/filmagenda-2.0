13e21831782b2c54368cc0301f0ec0d9913b1072
jlsears <jen.lauren.sears@gmail.com>
2016-08-04 22:15:41 -0500

    tidying up

--- js/controllers/faveperformerscontroller.js
+++ js/controllers/faveperformerscontroller.js
@@ -36,19 +36,11 @@
 
     $scope.saveFire = function(artist) {
 
-      //$scope.most_recent = $scope.getTitle1;
       $scope.artist_type = 'Experiment NOW';
 
       var item = artistListing.$getRecord($scope.artist.$id);
-      //console.log("artistListing: " + JSON.stringify(artistListing));
-      console.log("artist inside saveFire: " + JSON.stringify(artist));
-      console.log("and: " + item);
 
-      //item.artist_type = 'experimenting NOW';
-
-      console.log("ultimate title in saveFire: " + $scope.getTitle2);
-
-      $scope.artist.arist_type = 'experimenting NOW'
+      $scope.artist.artist_type = 'experimenting NOW'
       $scope.artist.most_recent = $scope.getTitle2
       //$scope.artist.media_type = $scope.format_type
 
@@ -65,7 +57,6 @@
       var getTitle2;
       var format_type;
       var format_type1;
-      //var titleUse = "unchanged";
       var titleinfo = "not changed";
       var winningTitle = "";
 
@@ -96,20 +87,11 @@
               // cast variables
               var castData = moredata.cast[0].original_title;
 
-              //console.log("castData: " + castData);
-              //console.log("moredata: " + JSON.stringify(moredata));
-
               var lastItem = moredata.cast.length - 1;
-              //var lastItem2 = moredata.crew.length - 1;
-
-              // Comparing first air dates
 
               console.log("before loop: " + moredata.cast[0].release_date);
 
               for(var i = 0; i < lastItem-1; i++) {
-                //console.log("inside for loop");
-
-                //var finished = 0;
 
                 var firstMovie = moredata.cast[i].media_type;
                 var secMovie = moredata.cast[i+1].media_type;
@@ -119,51 +101,14 @@
                 var firstDate = moredata.cast[i].release_date;
                 var secDate = moredata.cast[i+1].release_date;
 
-                //console.log("firstDate: " + firstDate);
-                //console.log("secDate: " + secDate);
-
                 if(firstDate > secDate) {
-                  //console.log("inside if statement: " + moredata.cast[i].original_title);
 
                   $scope.getTitle2 = moredata.cast[i].original_title;
-                  //$scope.format_type = moredata.cast[i].media_type || null;
-                  console.log("winning title is: " + $scope.getTitle2);
 
-                }
-                //console.log("artist in the loop: " + JSON.stringify(artist));
-
-/*                if(i == lastItem-2); {
-                  $scope.saveFire(artist);
-                }
-*/              
+                  }
                 }
               } // end for loop
-              
-
-              //console.log("artist: " + JSON.stringify(artist.name));
-              //console.log("the moredata: " + JSON.stringify(moredata));
-              //console.log("crew title: " + JSON.stringify(moredata.crew[lastItem2].title));
-              //console.log("crew media type: " + JSON.stringify(moredata.crew[lastItem2].media_type));
-              //console.log("the lastItem2: " + lastItem2);
-
-              // Will be present for crew data
-              //$scope.getTitle1 = moredata.crew[lastItem2].title;
-              //$scope.format_type1 = moredata.crew[lastItem2].media_type;
-
-              // Will be present for cast data
-              //$scope.getTitle2 = moredata.cast[lastItem].original_name;
-              //$scope.format_type = moredata.cast[lastItem].media_type;
-
-              // This means the project is a TV show
-/*              if (typeof getTitle1 === 'undefined') {
-                titleinfo = moredata.cast[lastItem].original_name;
-                $scope.artist = artist;
-                $scope.most_recent = titleinfo;
-
-              } else {
-                //project is a film scenario here                 
-              }
-*/            },
+            },
           })
 
           getAll2.then($scope.saveFire);
